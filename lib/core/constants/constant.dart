a493e5c07b11b1579a4a7f4d826ef332096d9d38
Xusniddin2000 <83164186+Xusniddin2000@users.noreply.github.com>
2023-06-17 11:06:19 +0500

    Initial commit

---
+++ lib/core/constants/constant.dart
@@ -0,0 +1,1 @@
+class  AppConstants{  static const  String baseUrl="http://192.168.1.101:8081/api";  //receive Timeout   static   const  int  receiveTimeout=5000; // connection  Timeout  static  const  int  connectionTimeout=5000;  static  const String  users='/user';}
